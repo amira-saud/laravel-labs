e3342931543518b60862509401427bf5b80dda6a
Amira <miramiromiral@gmail.com>
2018-04-14 00:33:44 +0200

    show data in table

--- myproject/resources/views/posts/index.blade.php
+++ myproject/resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
-
-
 @section('content')
+<!--
+
 
 <h1>Posts Index</h1>
 <ul>
@@ -16,5 +16,35 @@
 
 
 </ul>
+-->
+
+
+
+    <h1>Posts Data</h1>
+        <table class="table table-striped">
+        <th><strong> Post Title </strong></th>
+        <th><strong> Posted By </strong></th>
+        <th><strong> Created At </strong></th>
+        <th><strong> Actions </strong></th>
+
+        @foreach ($posts as $post)
+        <tr>
+               
+
+        
+                
+
+<td> {{ $post->title }} </td>
+<td> {{ $post->user->name}} </td>
+<td> {{ $post->created_at }} </td>
+<td>    
+<a href="#" ><button>View</button></a>
+    <a href="#" ><button>Edit</button></a>
+        <a href="#"><button>Delete</button></a>
+
+@endforeach
+
+        </tr>
+        </table>
 
 @endsection
